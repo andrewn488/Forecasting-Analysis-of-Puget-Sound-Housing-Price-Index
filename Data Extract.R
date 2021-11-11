ead42c3c836d4f6bf2d3901459139b3e305e4647
jsburns13 <86275366+jsburns13@users.noreply.github.com>
2021-11-11 00:14:53 -0800

    Update Data Extract.R
    Create post-pandemic view and acf/pacf

--- Data Extract.R
+++ Data Extract.R
@@ -3,10 +3,9 @@ library(tidyverse)
 
 housing <- read_csv("fmhpi_master_file.csv")
 
-#getwd()
-
 summary(housing)
 
+#create table of Seattle-Tacoma-Bellevue area
 puget <- filter(housing, GEO_Name == 'Seattle-Tacoma-Bellevue WA')
 
 puget$YearMonth <- as.Date(with(puget,paste(Year,Month,1,sep="-")),"%Y-%m-%d")
@@ -20,3 +19,12 @@ ggplot(data = puget) +
 
 acf(puget[, c('YearMonth', 'Index_NSA')], lag.max=20)
 pacf(puget[, c('YearMonth', 'Index_NSA')], lag.max=20)
+
+#drill down to pandemic-influenced pricing
+PandemicPricing <- subset(puget, YearMonth>"2019-12-31")
+
+ggplot(data = PandemicPricing) +
+  geom_line(mapping = aes(x = YearMonth, y = Index_NSA, group = GEO_Name))
+
+acf(PandemicPricing[, c('YearMonth', 'Index_NSA')], lag.max=18)
+pacf(PandemicPricing[, c('YearMonth', 'Index_NSA')], lag.max=18)
